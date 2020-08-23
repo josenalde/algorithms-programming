7ac66359e0cb93293aab29d2d1c0b300babd6725
josenalde <josenalde@gmail.com>
2020-08-23 15:49:27 -0300

    string, getline

---
+++ src/s9.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+int main() {
+   system("cls");
+   std::string name;
+   std::cout << "Please, enter your full name: ";
+   std::getline(std::cin,name);
+   std::cout << "Hello, " << name << "!\n";
+   return 0;
+}
+
+
+  
