3ff9c2eb2e08b4f05bddfbab17f7fb53e4eae29d
josenalde <josenalde@gmail.com>
2020-08-23 15:16:35 -0300

    print special characters using backslash

--- src/s2.cpp
+++ src/s2.cpp
@@ -3,11 +3,11 @@
 #include <iostream>
 using std::cout;
 using std::cin;
-//using std::endl
+// using std::endl // *
 
 int main() {
-    // it shows special characters
-    cout << "UFRN\\EAJ" << std::endl; // just endl with header using
+    // it shows special characters using backslash
+    cout << "UFRN\\EAJ" << std::endl; // * just endl with header using
     cout << "OLIVEIRA\"JOSENALDE\"" << std::endl;
     cout << "\'BARRA SIMPLES\'";
     
