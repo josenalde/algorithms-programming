6826aa4ab1fb97453c43f7dc898d392c174e5e58
josenaldeOliveira <josenalde@gmail.com>
2020-10-05 11:53:34 -0300

    with stl vector and sort algorithm

--- src/s18.cpp
+++ src/s18.cpp
@@ -1,27 +1,37 @@
 
 
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
+#include <ctime>   // for time, clock_t
+#include <cstdlib> // for rand(), srand()
+#include <algorithm> // for sort()
+#include <vector> // STL Vector
+
 
 #define MIN (1.50f)
 #define MAX (4.50f)
 
 int comp(const void *, const void *);
+bool stlcomp(double n1, double n2);
 void ordenaDec(double [], unsigned int);
 using namespace std;
 
 int main() {
     srand(time(NULL));
     clock_t start_t, end_t;
+    
     unsigned int sV = 1e3;
     double x[sV];
+    vector<double> y(sV);
+    
     for (int i = 0; i < sV; i++) {
         x[i] = MIN + (rand() / (RAND_MAX / (MAX-MIN)));
+        y[i] = x[i]; //also adds elements to STL vector
     }
     start_t = clock();
     // switch these two lines below to compare sorting times
     //qsort(x, sV, sizeof(double), comp); //quick sort
+    //sort(y.begin(), y.end())
+    //sort(y.begin(), y.end(), stlcomp);
     ordenaDec(x, sV); //insertion sort (based on exchange)
     end_t = clock();
     double total_t = (double)(end_t-start_t)/(double)CLOCKS_PER_SEC;
@@ -46,4 +56,8 @@ void ordenaDec(double v[], unsigned int tam) {
             i = -1; // forced reset
         }
     }
+}
+
+bool stlcomp(double n1, double n2) {
+    return (n1 > n2);
 }
